54306cfb909946392e09e1c044ef3b44ab0b6acf
JFCasas <fernando.casas@gmail.com>
2018-02-20 11:57:31 +0100

    CREACION DE PRIMER COMPONENTE Y USO DEL MISMO

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Title from './components/Title'
+
 class App extends Component {
 
   
@@ -11,8 +13,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
           
+          <Title></Title> 
           
         </header>
 
---
+++ src/components/Title.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class Title extends React.Component {
+
+	render(){
+
+		return (
+
+			<div>
+
+				<h1>Places</h1>
+				<p>Descubre lugares de manera simple</p>
+				
+
+			</div>
+		
+		)
+	}
+
+
+}
