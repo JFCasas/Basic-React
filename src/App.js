3f7a63a7317d59828eb0cddefb8a4acfb4920e4f
JFCasas <fernando.casas@gmail.com>
2018-02-24 13:33:41 +0100

    IMPLEMENTACION DE MATERIAL-DESIGN

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+
+
+import {indigo400,red500, yellow500, blue500, grey50} from 'material-ui/styles/colors'
+
+
+import Title from './components/Title'
+
 import logo from './logo.svg';
 import './App.css';
 
-import Title from './components/Title'
 
 class App extends Component {
 
@@ -30,47 +40,77 @@ class App extends Component {
   }
 
   render() {
+    
     return (
 
-      <section>
+      <MuiThemeProvider>
   
         <div className = "Header-background">
           
-          <div style={{ "width":"80%", "margin" : "0 auto", "background-color": "#eee"}} >
+          <div  className = "container" style={{ "width":"70%", "margin" : "0 auto"}} >
 
-            <Title></Title> 
-            
-            <h2>{this.state.numero}</h2>
-            
-            <button onClick={this.updateNumero}  >Crear Cuenta Gratuita</button>
-            
-            <img src={process.env.PUBLIC_URL + '/images/places1.jpeg'} height="200"/>
-            
-            <div>
-      
-              <ul>
-                
-                <li>
-                  
-                  <h3>Calificaciones con emociones</h3>
-                  <p>Califica tus lugares con experiencias, no con números</p>
-                
-                </li>
-
-                <li>
-                  
-                  <h3>No Internet. No problemas</h3>
-                  <p>Esta página funciona sin Internet o en conexiones lentas</p>
-                
-                </li>
-
-                <li>
-                  
-                  <h3>Tus lugares favoritos</h3>
-                  <p>Define tu lista de sitios favoritos</p>
-                
-                </li>
+            <div className = "titulo">
               
+              <Title></Title> 
+            
+              <RaisedButton label="Crear Cuenta Gratuita" secondary={true} onClick={this.updateNumero}  />
+
+            </div>
+
+            
+            <div style={{"background-color":grey50, "padding":"5em","margin-top":"1em"}}>
+      
+              <ul className = "lista">
+                
+                <Card style={{"font-family":"'Roboto', sans-serif"}}>
+                  <CardHeader  style={{"font-weight":"bold"}}
+
+                    title="Calificaciones con emociones"
+                    avatar="images/experiencias.jpeg"
+                    
+                  />
+                  <CardActions>
+                    <FlatButton label="Action1" />
+                    <FlatButton label="Action2" />
+                  </CardActions>
+                  <CardText >
+
+                    
+                    Califica tus lugares con experiencias, no con números
+                  
+                  </CardText>
+                </Card>
+
+                <Card style={{"font-family":"'Roboto', sans-serif"}}>
+                  <CardHeader  style={{"font-weight":"bold"}}
+                    title="No Internet. No problemas"
+                    avatar="images/internet.jpeg"
+                    
+                  />
+                  <CardActions>
+                    <FlatButton label="Action1" />
+                    <FlatButton label="Action2" />
+                  </CardActions>
+                  <CardText >
+                    Esta página funciona sin Internet o en conexiones lentas
+                  </CardText>
+                </Card>
+
+                <Card style={{"font-family":"'Roboto', sans-serif"}}>
+                  <CardHeader  style={{"font-weight":"bold"}}
+                    title="Tus lugares favoritos"
+                    avatar="images/favoritos.jpeg"
+                    
+                  />
+                  <CardActions>
+                    <FlatButton label="Action1" />
+                    <FlatButton label="Action2" />
+                  </CardActions>
+                  <CardText >
+                    Define tu lista de sitios favoritos
+                  </CardText>
+                </Card>
+                            
               </ul>
             
             </div>
@@ -79,7 +119,7 @@ class App extends Component {
         
         </div>
 
-      </section>
+      </MuiThemeProvider>
       
 
 
