d4b5510c23b55e79903e1064e5fd7d5cb968ca6b
JFCasas <fernando.casas@gmail.com>
2018-03-05 17:56:31 +0100

    CONFIGURACION PAGINA DE LOGIN

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,78 @@
 import React from 'react';
 
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import {orange500,grey900 } from 'material-ui/styles/colors';
+
+const styles = {
+  errorStyle: {
+    color: orange500,
+  },
+  underlineStyle: {
+    borderColor: grey900,
+  },
+  floatingLabelStyle: {
+    color: grey900,
+  },
+  
+};
+
+
+
 export default class Login extends React.Component {
 
 	render(){
 
 		return (
 
-			<h1>Login</h1>
+			<div className = "formulario" style ={{'backgroundImage' : "url("+process.env.PUBLIC_URL + '/images/fondologin.jpeg'+")"}} >
+
+				<div >
+
+					<TextField 
+      				
+      					floatingLabelText="Email"
+      					type="email"
+      					style = {{"width" : "30vw"}}
+      					underlineStyle={styles.underlineStyle}
+      					underlineFocusStyle={styles.underlineStyle}
+      					floatingLabelStyle={styles.floatingLabelStyle}
+    				/>
+					
+
+				</div>
+
+				<div >
+
+					<TextField 
+      				
+      					floatingLabelText="Password"
+      					type="password"
+      					style = {{"width" : "30vw", "marginTop" : "5em"}}
+      					underlineStyle={styles.underlineStyle}
+      					underlineFocusStyle={styles.underlineStyle}
+      					floatingLabelStyle={styles.floatingLabelStyle}
+    				/>
+					
+
+				</div>
+
+				<div >
+
+					<RaisedButton label="Login" secondary={true} 
+						style = {{"width" : "10vw", "marginTop" : "5em"}}
+					/>
+					
+
+				</div>
+
+				
+
+			</div>
 
 			
+
 		)
 	}
 
